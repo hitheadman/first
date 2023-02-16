a7de42ab40c32433fe2f9f356acc00c6da6f4d8c
Vladimir <123391267+hitheadman@users.noreply.github.com>
2023-02-16 22:13:40 +0500

    Add files via upload

---
+++ DataType.py
@@ -0,0 +1,7 @@
+per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+money = int(input("Money = "))
+for key in per_cent:
+    per_cent[key] = money/100*per_cent[key]
+deposite = [round(x, 2) for x in list(per_cent.values())]
+print(deposite)
+print("Максимальная сумма, которую вы можете заработать — ", max(deposite))
